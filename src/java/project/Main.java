db7c2393db2ed8c94f7d1c2a421330909c64389c
Mari_023 <Mari_023@outlook.de>
2021-06-14 10:10:01 +0200

    add main class and begin config package

---
+++ src/java/project/Main.java
@@ -0,0 +1,12 @@
+package project;
+
+public class Main {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/java/project/config/api/Serializable.java
@@ -0,0 +1,4 @@
+package project.config.api;
+
+public interface Serializable {
+}
