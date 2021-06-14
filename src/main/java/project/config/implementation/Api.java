958d0d7dad708a87b4336616122938372d97f59d
Mari_023 <Mari_023@outlook.de>
2021-06-14 11:12:53 +0200

    some more implementation

---
+++ src/main/java/project/config/implementation/Api.java
@@ -0,0 +1,37 @@
+package project.config.implementation;
+
+import project.config.api.Serializable;
+import project.config.api.Storage;
+
+public class Api extends project.config.api.Api {
+
+    @Override
+    public Storage saveObject(Serializable serializable) {
+        return null;
+    }
+
+    @Override
+    public Storage saveInt(int i) {
+        return null;
+    }
+
+    @Override
+    public Storage saveBoolean(boolean b) {
+        return null;
+    }
+
+    @Override
+    public Storage saveDouble(double d) {
+        return null;
+    }
+
+    @Override
+    public Storage saveFloat(float f) {
+        return null;
+    }
+
+    @Override
+    public Storage saveString(String s) {
+        return null;
+    }
+}
--- src/main/java/project/config/implementation/ObjectStorage.java
+++ src/main/java/project/config/implementation/ObjectStorage.java
@@ -1,4 +1,18 @@
 package project.config.implementation;
 
+import project.config.api.Serializable;
+import project.config.api.Storage;
+
+import java.util.HashMap;
+
 class ObjectStorage {
+    private final HashMap<String, Storage> serializedObject;
+
+    public ObjectStorage(Serializable serializable) {
+        serializedObject = serializable.serialize();
+    }
+
+    protected HashMap<String, Storage> getSerializedObject() {
+        return serializedObject;
+    }
 }
