03499bc9b64b0050443bda3f0494b86b73c1b077
Jan <75756634+Specktulatius@users.noreply.github.com>
2021-06-21 17:51:54 +0200

    configured the Liti Engine with the gameInfo.xml testing Game.graphics().*(); in GameState.java

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -2,29 +2,15 @@ package project;
 
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.GameListener;
+import project.logic.GameState;
 
 public class Main {
 
     /**
-     *
      * @param args
      */
     public static void main(String[] args) {
-        Game.addGameListener(new GameListener() {
-            @Override
-            public void initialized(String... args) {
-                // do sth when game is initialized
-            }
-            @Override
-            public void started() {
-                // do sth when game started
-            }
-
-            @Override
-            public void terminated() {
-                // do sth when game terminated
-            }
-        });
+        Game.addGameListener(new GameState());
         Game.init(args);
         Game.start();
     }
