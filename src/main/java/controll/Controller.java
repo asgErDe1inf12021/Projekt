a61182e120f94c345bcf020f1e50fc21bca819ff
Specktulatius <janspeckter@live.de>
2021-06-21 11:14:07 +0200

    restructured project for use with LitiEngine

--- src/main/java/controll/Controller.java
+++
@@ -1,4 +0,0 @@
-package controll;
-
-public class Controller {
-}
--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,5 +1,8 @@
 package project;
 
+import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.GameListener;
+
 public class Main {
 
     /**
@@ -7,6 +10,22 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
+        Game.addGameListener(new GameListener() {
+            @Override
+            public void initialized(String... args) {
+                // do sth when game is initialized
+            }
+            @Override
+            public void started() {
+                // do sth when game started
+            }
 
+            @Override
+            public void terminated() {
+                // do sth when game terminated
+            }
+        });
+        Game.init(args);
+        Game.start();
     }
 }
