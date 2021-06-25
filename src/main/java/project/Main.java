67a94558401208b681ba213205d9e3cec2a03560
Mari_023 <Mari_023@outlook.de>
2021-06-25 12:01:45 +0200

    optimize imports

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,9 +1,8 @@
 package project;
 
 import de.gurkenlabs.litiengine.Game;
-import de.gurkenlabs.litiengine.GameListener;
-import project.logic.GameState;
 import project.config.api.Api;
+import project.logic.GameState;
 
 public class Main {
 
