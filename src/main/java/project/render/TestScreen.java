3d3ee59004bf66d21179d8d8ec2397b69d2846dd
Mari_023 <Mari_023@outlook.de>
2021-06-28 11:10:00 +0200

    show fps instead of "Test text"

--- src/main/java/project/render/TestScreen.java
+++ src/main/java/project/render/TestScreen.java
@@ -1,5 +1,6 @@
 package project.render;
 
+import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.graphics.TextRenderer;
 import de.gurkenlabs.litiengine.gui.screens.GameScreen;
 
@@ -13,8 +14,7 @@ public class TestScreen extends GameScreen {
     @Override
     public void render(final Graphics2D g) {
         super.render(g);
-        g.setFont(g.getFont().deriveFont(50f));
         g.setColor(Color.white);
-        TextRenderer.render(g, "Test text", 100, 100);
+        TextRenderer.render(g, ""+Game.loop().getTickRate(), 5, 15);
     }
 }
