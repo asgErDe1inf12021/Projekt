65e6e9b3173bf3a14e22a97cfce7ae9264d6e5bf
Mari_023 <Mari_023@outlook.de>
2021-07-05 10:58:53 +0200

    set collision size

--- src/main/java/project/entities/Player.java
+++ src/main/java/project/entities/Player.java
@@ -1,10 +1,14 @@
 package project.entities;
 
+import de.gurkenlabs.litiengine.entities.CollisionInfo;
 import de.gurkenlabs.litiengine.entities.Creature;
 import de.gurkenlabs.litiengine.entities.EntityInfo;
+import de.gurkenlabs.litiengine.entities.MovementInfo;
 import project.input.KeyboardEntityController;
 
 @EntityInfo(width = 132, height = 140)
+@MovementInfo(velocity = 100)
+@CollisionInfo(collisionBoxWidth = 132, collisionBoxHeight = 140, collision = true)
 public class Player extends Creature {
 
     public Player() {
