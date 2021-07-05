fade94c055513931bb31e95e478505ad6453eeeb
Mari_023 <Mari_023@outlook.de>
2021-07-05 10:50:09 +0200

    some test sprites

---
+++ src/main/resources/player.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.03.23" name="player" tilewidth="32" tileheight="32" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="32" height="32" source="player.png"/>
+ </tile>
+</tileset>
