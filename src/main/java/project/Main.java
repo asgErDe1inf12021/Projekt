36322383df66221566164a97dcbee3c44ecd75b4
Mari_023 <Mari_023@outlook.de>
2021-07-09 11:50:18 +0200

    try fixing stuff

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,10 +1,14 @@
 package project;
 
 import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.environment.tilemap.xml.Tileset;
+import de.gurkenlabs.litiengine.graphics.Spritesheet;
 import de.gurkenlabs.litiengine.resources.Resources;
 import project.entities.Player;
 import project.logic.GameState;
 import project.save.api.Api;
+
+import java.awt.image.BufferedImage;
 
 public class Main {
 
@@ -17,6 +21,7 @@ public class Main {
         Game.init(args);
         Game.world().loadEnvironment("test.tmx");
         Game.start();
+        Resources.load("player.tsx");
         Game.world().environment().add(new Player());
         //Game.world().camera().pan(10000, 10000, 1000);
     }
