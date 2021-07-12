79c9b7a240846d2f42c681d8cfd222515d9fa37f
Mari_023 <Mari_023@outlook.de>
2021-07-12 10:25:38 +0200

    scale player and align correctly

--- src/main/java/project/entities/Player.java
+++ src/main/java/project/entities/Player.java
@@ -1,5 +1,7 @@
 package project.entities;
 
+import de.gurkenlabs.litiengine.Align;
+import de.gurkenlabs.litiengine.Valign;
 import de.gurkenlabs.litiengine.entities.CollisionInfo;
 import de.gurkenlabs.litiengine.entities.Creature;
 import de.gurkenlabs.litiengine.entities.EntityInfo;
@@ -8,12 +10,13 @@ import project.input.KeyboardEntityController;
 
 @EntityInfo(width = 132, height = 140)
 @MovementInfo(velocity = 100)
-@CollisionInfo(collisionBoxWidth = 132, collisionBoxHeight = 140, collision = true)
+@CollisionInfo(collisionBoxWidth = 132/4, collisionBoxHeight = 140/4, collision = true, align = Align.CENTER, valign = Valign.MIDDLE)
 public class Player extends Creature {
 
     public Player() {
         super("player");
         setVisible(true);
+        animations().scaleSprite(0.25F);
         KeyboardEntityController<Player> keyboard = new KeyboardEntityController<>(this);
         addController(keyboard);
         //addEntityRenderListener(new PlayerRenderListener());
