958aef2f2ec7b923ed890303ca8bb72e17bd7334
Specktulatius <janspeckter@live.de>
2021-07-12 11:11:51 +0200

    added stone Tileset and Map

---
+++ src/main/resources/Stone-Tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.03.23" name="Stone-Tile" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../namenloses Projekt/src/main/resources/Stone-Tile.png" width="32" height="32"/>
+</tileset>
