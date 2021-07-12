7f90a818cacf2380d2b5ecd0cf40f5a944b571d2
arn.langheld <alksdjfh@aslkdjf.com>
2021-07-12 11:05:44 +0200

    first baby steps with GUI editor

---
+++ src/main/java/project/GUI/main_menu/menu.java
@@ -0,0 +1,11 @@
+package project.GUI.main_menu;
+
+import javax.swing.*;
+
+public class menu {
+    private JButton startButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
