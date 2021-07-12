772aa94d4fb92121d06b7435c6b4a2ea73ad744c
Jan <75756634+Specktulatius@users.noreply.github.com>
2021-07-12 19:21:52 +0200

    edited testing map

--- src/main/resources/stoneMap.tsx
+++ src/main/resources/stoneMap.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.1" name="test" tilewidth="16" tileheight="16" tilecount="5" columns="0">
+<tileset version="1.5" tiledversion="1.7.1" name="test" tilewidth="16" tileheight="16" tilecount="4" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="2">
   <image width="16" height="16" source="stone.png"/>
@@ -9,5 +9,8 @@
  </tile>
  <tile id="4">
   <image width="16" height="16" source="planks_oak.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="bedrock.png"/>
  </tile>
 </tileset>
