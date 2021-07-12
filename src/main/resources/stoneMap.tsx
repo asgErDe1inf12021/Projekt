9b84465a6f51d58882fae722d450e8cc48755a5c
Jan <75756634+Specktulatius@users.noreply.github.com>
2021-07-12 19:33:46 +0200

    fixed testing map (tilecount)

--- src/main/resources/stoneMap.tsx
+++ src/main/resources/stoneMap.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.1" name="test" tilewidth="16" tileheight="16" tilecount="4" columns="0">
+<tileset version="1.5" tiledversion="1.7.1" name="test" tilewidth="16" tileheight="16" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="2">
   <image width="16" height="16" source="stone.png"/>
