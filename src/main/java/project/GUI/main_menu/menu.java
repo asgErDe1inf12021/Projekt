8919b0b5a9f4e55640135ff3713310a9ef31a5a4
arn.langheld <alksdjfh@aslkdjf.com>
2021-07-16 12:03:06 +0200

    added ActionListener for start button, attempt to integrate GUI into main.

--- src/main/java/project/GUI/main_menu/menu.java
+++ src/main/java/project/GUI/main_menu/menu.java
@@ -1,9 +1,21 @@
 package project.GUI.main_menu;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class menu {
     private JButton startButton;
+    private JTextField textField1;
+
+    public menu() {
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,15 +1,16 @@
 package project;
 
 
+import project.GUI.main_menu.menu;
 import project.config.api.Api;
 
 public class Main {
-
     /**
      *
      * @param args
      */
-    public static void main(String[] args) {
+    public static <M> void main(String[] args, menu M) {
         Api.Api = new project.config.implementation.Api();
+
     }
 }
